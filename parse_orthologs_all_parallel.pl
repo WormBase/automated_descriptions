0f0783be75e462bab20677cfe439f79b628c271e
james done <jdone@wormbase.org>
2015-07-30 16:28:26 -0700

    Update for WS250

---
+++ parse_orthologs_all_parallel.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use ConciseDescriptions;
+use File::Slurp;
+#
+my @args;
+my $status;
+my $html = ConciseDescriptions::get_html_dir();
+my $home = $html . "concise_descriptions/";
+my $script = "./parse_orthologs_species_parallel.pl";
+my $parallel_file = "./parallel_path.txt";
+my $parallel_path = read_file($parallel_file);
+ chomp($parallel_path);
+ $parallel_path =~s/^\s+//;
+ $parallel_path =~s/\s+$//;
+my $parallel_exec = $parallel_path . "parallel";
+my $three_colons = "\:\:\:";
+my $j_flag = "\-j";
+my $percent = "85\%";
+#
+# $species_file holds a list of species and project numbers studied by Wormbase.
+my $species_file = $home . "species.txt";
+my @lines = read_file($species_file);
+#
+my @species_array = ();
+foreach my $line (@lines){
+ chomp($line);
+ my ($species, $project, $name, $prefix) = split(/\t/,$line);
+ $species =~s/^\s+//;
+ $species =~s/\s+$//;
+ $project =~s/^\s+//;
+ $project =~s/\s+$//;
+ $name =~ s/^\s+//;
+ $name =~ s/\s+$//;
+ $name =~s/\s/\_/g;
+ my $term = $species . "AND" . $project ."AND" . $name;
+ push(@species_array, $term);
+}
+ @args=($parallel_exec, $j_flag, $percent, $script, $three_colons, @species_array);
+ $status=system(@args);
+#
+exit 0;
