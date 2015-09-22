a4db6a3e33ddc1525ad0aa47269c3fd0cc7e212e
james done <jdone@wormbase.org>
2015-09-22 14:12:49 -0700

    Update for WS251

--- create_sentence_multiple_orthologs_parasites.pl
+++ create_sentence_multiple_orthologs_parasites.pl
@@ -10,8 +10,7 @@ use strict;
 my $species = $ARGV[0];
 my $project = $ARGV[1];
 my $name = $ARGV[2];
-my $prefix= $ARGV[3];
-
+my $prefix = $ARGV[3];
 chomp($species);
 chomp($project);
 chomp($name);
@@ -21,10 +20,10 @@ $project =~ s/^\s+//;
 $project =~ s/\s+$//;
 $species =~ s/^\s+//;
 $species =~ s/\s+$//;
-$name    =~ s/^\s+//;
-$name    =~ s/\s+$//;
-$prefix  =~ s/^\s+//;
-$prefix  =~ s/\s+$//;
+$name =~ s/^\s+//;
+$name =~ s/\s+$//;
+$prefix =~ s/^\s+//;
+$prefix =~ s/\s+$//;
 
 my $RELEASE = ConciseDescriptions::get_release();
 my $PRODUCTION_RELEASE = ConciseDescriptions::get_production_release();
@@ -32,9 +31,9 @@ my $html = ConciseDescriptions::get_html_dir();
 my $home = $html . "concise_descriptions/";
 # The list of genes with no concise descriptions is in $infile
 my $elegans_gene_list_dir = $home . "release/$PRODUCTION_RELEASE/c_elegans/gene_lists/";
+my $curated_gene_list = $elegans_gene_list_dir . "sort.curated_genes.txt";
 my $db_gene_list  = $elegans_gene_list_dir . "wb_gene_list.txt";
 
-my $curated_gene_list = $elegans_gene_list_dir . "sort.curated_genes.txt";
 my @curated_genes = read_file($curated_gene_list);
 
 my $orthology = $home . "release/$PRODUCTION_RELEASE/$species/orthology/";
@@ -54,6 +53,24 @@ my $orthology_elegans = $orthology_elegans_dir . "input_files/c_elegans.ortholog
 my $orthology_elegans_species = $orthology . "input_files/orthologs.$species.elegans.txt";
 my $orthology_species = $orthology . "input_files/$species.orthologs.txt";
 my $output_file = $orthology . "output_files/sentences_for_orthology.txt";
+#
+my $parasite_orthologs = "";
+my $is_a_parasite = "";
+my $second_parasite_orthologs = "";
+my $is_a_second_parasite = "";
+#
+if ($species =~/volvulus/){
+  $parasite_orthologs = $orthology . "input_files/orthologs.o_volvulus.malayi.txt";
+  $is_a_parasite = " is an ortholog of B\. malayi ";
+} elsif ($species =~/malayi/){
+  $parasite_orthologs = $orthology . "input_files/orthologs.b_malayi.volvulus.txt";
+  $is_a_parasite = " is an ortholog of O\. volvulus ";
+} elsif ($species =~/ratti/){
+  $parasite_orthologs = $orthology . "input_files/orthologs.s_ratti.volvulus.txt";
+  $is_a_parasite = " is an ortholog of O\. volvulus ";
+  $second_parasite_orthologs = $orthology . "input_files/orthologs.s_ratti.malayi.txt";
+  $is_a_second_parasite = " is an ortholog of B\. malayi ";
+}
 #
 if (-e $orthology){
    print "$orthology exists\n"; 
@@ -78,10 +95,17 @@ if (-e $output_file){
       my @args = ("/bin/rm", "$individual");
       system(@args) == 0 or die("could not delete $individual\n");
    }
-
 my ($gene_elegans_id_hash_ref, $gene_elegans_hash_ref) = get_id_name_hash($orthology_elegans_species, $db_gene_list);
 my %gene_elegans_hash= %{$gene_elegans_hash_ref};
 my %gene_elegans_id_hash= %{$gene_elegans_id_hash_ref};
+
+my ($gene_parasite_id_hash_ref, $gene_parasite_hash_ref) = get_id_name_hash($parasite_orthologs, $db_gene_list);
+my %gene_parasite_hash= %{$gene_parasite_hash_ref};
+my %gene_parasite_id_hash= %{$gene_parasite_id_hash_ref};
+
+my ($gene_second_parasite_id_hash_ref, $gene_second_parasite_hash_ref) = get_id_name_hash($second_parasite_orthologs, $db_gene_list);
+my %gene_second_parasite_hash= %{$gene_second_parasite_hash_ref};
+my %gene_second_parasite_id_hash= %{$gene_second_parasite_id_hash_ref};
 
 my $gene_name_hash_ref = get_gene_name_hash($orthology_species, $db_gene_list);
 my %gene_name_hash = %$gene_name_hash_ref;
@@ -133,7 +157,7 @@ foreach my $test (@sorted_uncurated_genes_array){
    push(@uncurated_live_genes_array, $test);
  }
 }
-my @sorted_uncurated_live_genes_array = uniq(sort(@uncurated_live_genes_array)); 
+my @sorted_uncurated_live_genes_array = sort(@uncurated_live_genes_array); 
  
 foreach my $gene_id (@sorted_uncurated_live_genes_array){
   my $sentence  = "";
@@ -142,9 +166,7 @@ foreach my $gene_id (@sorted_uncurated_live_genes_array){
        $gene_id =~ s/\s+$//;
   my $out = $individual_path . $gene_id;
   my $gene_name = $gene_name_hash{$gene_id};
-#
-# Fill array, @elegans_array, with list of WBGeneID orthologs.
-#
+
   my $elegans_list = "";
   my @elegans_array = ();
   if ($gene_elegans_id_hash{$gene_id}){
@@ -155,13 +177,9 @@ foreach my $gene_id (@sorted_uncurated_live_genes_array){
      $elegans_array[0] = $elegans_list;
    }
   }
-
   my $elegans_phrase = "";
   my $count_elegans = 0;
   my $elegans_size = @elegans_array;
-#  print "$gene_name\t$elegans_size\n";
-  if ($elegans_size < 6){
-
   foreach my $elegans (@elegans_array){
        $elegans =~ s/^\s+//;
        $elegans =~ s/\s+$//;
@@ -177,30 +195,100 @@ foreach my $gene_id (@sorted_uncurated_live_genes_array){
    }
     $count_elegans++;
   } # end foreach my $elegans
+  my $parasite_list = "";
+  my @parasite_array = ();
+  if ($gene_parasite_id_hash{$gene_id}){
+      $parasite_list = $gene_parasite_id_hash{$gene_id};
+   if ($parasite_list =~/$AND/){
+      @parasite_array = split(/$AND/, $parasite_list);
+    } else {
+     $parasite_array[0] = $parasite_list;
+   }
+  }
+  my $second_parasite_list = "";
+  my @second_parasite_array = ();
+  if ($gene_second_parasite_id_hash{$gene_id}){
+      $second_parasite_list = $gene_second_parasite_id_hash{$gene_id};
+   if ($second_parasite_list =~/$AND/){
+      @second_parasite_array = split(/$AND/, $second_parasite_list);
+    } else {
+     $second_parasite_array[0] = $second_parasite_list;
+   }
+  }
 
-  } else {
-
-my $list = join(",",@elegans_array);
-#print "$gene_name\t$list\n";
+  my $parasite_phrase = "";
+  my $count_parasite = 0;
+  my $parasite_size = @parasite_array;
+  foreach my $parasite (@parasite_array){
+       $parasite =~ s/^\s+//;
+       $parasite =~ s/\s+$//;
+    my $gene_parasite = $gene_parasite_hash{$parasite};
+    if ($gene_parasite){
+    if ($count_parasite == 0){
+     $parasite_phrase = $gene_parasite;  
+    } elsif (($count_parasite > 0) and ($count_parasite < ($parasite_size -1))){
+     $parasite_phrase .= "\, " . $gene_parasite;  
+    } elsif (($count_parasite > 0) and ($count_parasite == ($parasite_size -1))){
+     $parasite_phrase .= " and " . $gene_parasite;  
+    }
+   }
+    $count_parasite++;
+  } # end foreach my $parasite
+  my $second_parasite_phrase = "";
+  my $count_second_parasite = 0;
+  my $second_parasite_size = @second_parasite_array;
+  foreach my $parasite (@second_parasite_array){
+       $parasite =~ s/^\s+//;
+       $parasite =~ s/\s+$//;
+    my $gene_parasite = $gene_second_parasite_hash{$parasite};
+    if ($gene_parasite){
+    if ($count_second_parasite == 0){
+     $second_parasite_phrase = $gene_parasite;  
+    } elsif (($count_second_parasite > 0) and ($count_second_parasite < ($second_parasite_size -1))){
+     $second_parasite_phrase .= "\, " . $gene_parasite;  
+    } elsif (($count_second_parasite > 0) and ($count_second_parasite == ($second_parasite_size -1))){
+     $second_parasite_phrase .= " and " . $gene_parasite;  
+    }
+   }
+    $count_second_parasite++;
+  } # end foreach my $parasite
+  if ($elegans_phrase) {
+   if ($elegans_size < 6) {
+      $sentence  = $gene_name . $is_a_ce . $elegans_phrase;
+      $sentence .= "\;\n";
+     } else {
+       my $list = join(",",@elegans_array);
+#       print "$gene_name\t$list\n";
 $sentence = $gene_name;
 $sentence = reduce_ortholog_sentence($sentence, $list,  $popularity_gene_id_name_hash_ref, $wormbase_gene_id_name_hash_ref,  $gene_elegans_hash_ref, $gene_elegans_id_hash_ref, $gene_name_hash_ref, $gene_class_hash_ref);
 $sentence .= "\;\n";
   }
-
-if (($elegans_phrase) and ($elegans_size < 6)) {
-      $sentence  = $gene_name . $is_a_ce . $elegans_phrase;
+ 
+      write_file($output_file, {append => 1 }, $sentence);
+      write_file($output_file, {append => 1 }, "\n\n");
+      write_file($out, $sentence);
+} elsif (($parasite_phrase) and ($second_parasite_phrase)){
+      $sentence  = $gene_name . $is_a_parasite . $parasite_phrase . " and" . $is_a_second_parasite . $second_parasite_phrase;
       $sentence .= "\;\n";
-}
-
-if ($sentence) {
+      write_file($output_file, {append => 1 }, $sentence);
+      write_file($output_file, {append => 1 }, "\n\n");
+      write_file($out, $sentence);
+} elsif ($parasite_phrase) {
+      $sentence  = $gene_name . $is_a_parasite . $parasite_phrase;
+      $sentence .= "\;\n";
+      write_file($output_file, {append => 1 }, $sentence);
+      write_file($output_file, {append => 1 }, "\n\n");
+      write_file($out, $sentence);
+} elsif ($second_parasite_phrase) {
+      $sentence  = $gene_name . $is_a_second_parasite . $second_parasite_phrase;
+      $sentence .= "\;\n";
       write_file($output_file, {append => 1 }, $sentence);
       write_file($output_file, {append => 1 }, "\n\n");
       write_file($out, $sentence);
 } 
- 
+
 } # end foreach my $gene_id (@gene_array)
 exit 0;
-
 sub reduce_ortholog_sentence{
 
 my $sentence=shift;
@@ -521,47 +609,76 @@ my @array=();
 
 sub get_id_name_hash{
  my $orthology=shift;
- my $db_gene_list  = shift;
+ my $db_gene_list = shift;
+ my %gene_name_hash=();
+ my %gene_id_hash=();
+
+ if ($orthology){
 
  my @lines = read_file($orthology);
  my $old_gene="";
- my %gene_name_hash=();
- my %gene_id_hash=();
  my @multiple_source_array=();
  my @single_source_array=();
  my $oldgene="";
  my $newgene; 
  foreach my $line (@lines){ 
     chomp($line);   
-    if ($line =~/WBGene/){
-     my ($wb_gene_id, $gene_name, $species_name, $id, $name, $source) = split(/\t/, $line); 
+    next if ($line !~/WBGene/);
+#     my ($wb_gene_id, $gene_name, $species_name, $id, $name, $source) = split(/\t/, $line); 
+       my ($wb_gene_id, $gene_species_name, $id, $name, $source) = split(/\t/, $line);
+       my ($gene_name, $species_name) = split(/ /, $gene_species_name);
+
         $wb_gene_id =~ s/^\s+//;
         $wb_gene_id =~ s/\s+$//;
-        $newgene = $wb_gene_id;
+        $name =~ s/^\s+//;
+        $name =~ s/\s+$//;
+        $source =~ s/^\s+//;
+        $source =~ s/\s+$//;
+
+        next if (($name eq "") or ($name eq "\."));
+        next if (($source eq "") or ($source eq "\."));
+
+         $newgene = $wb_gene_id;
      if ($newgene ne $oldgene){
          my $single_size = @single_source_array;
          my $multiple_size = @multiple_source_array;
 
-         if ($multiple_size > 0){
+         if ($multiple_size gt 0){
              foreach my $a_line (@multiple_source_array){
+               chomp($a_line);
                my ($a_wb_gene_id, $a_gene_species_name, $a_id, $a_name, $a_source) = split(/\t/, $a_line);
                my ($a_gene_name, $a_species_name) = split(/ /, $a_gene_species_name);
-                       $gene_name_hash{$a_id}  = $a_name;
- 
+                   $a_wb_gene_id =~ s/^\s+//;
+                   $a_wb_gene_id =~ s/\s+$//;
+                   $a_id =~ s/^\s+//;
+                   $a_id =~ s/\s+$//;
+                   $a_name =~ s/^\s+//;
+                   $a_name =~ s/\s+$//;
+                   next if (($a_name eq "") or ($a_name eq "\."));
+                   next if (($a_source eq "") or ($a_source eq "\."));
+                        $gene_name_hash{$a_id}  = $a_name;
+                   
                if ($gene_id_hash{$a_wb_gene_id}){
                    $gene_id_hash{$a_wb_gene_id} .= "AND" .  $a_id;
                } else{
                    $gene_id_hash{$a_wb_gene_id} = $a_id;
                }
             }
-          } elsif ($single_size > 0) {
+          } elsif ($single_size gt 0) {
              foreach my $a_line (@single_source_array){
+               chomp($a_line);
                my ($a_wb_gene_id, $a_gene_species_name, $a_id, $a_name, $a_source) = split(/\t/, $a_line);
+               next if (($a_name eq "") or ($a_name eq "\."));
+               next if (($a_source eq "") or ($a_source eq "\."));
+
                my ($a_gene_name, $a_species_name) = split(/ /, $a_gene_species_name);
-#                   $gene_name_hash{$a_id}  = $a_name;
-#                   if not ($gene_name_hash{$a_id}){
-                       $gene_name_hash{$a_id}  = $a_name;
-#                    } 
+                   $a_wb_gene_id =~ s/^\s+//;
+                   $a_wb_gene_id =~ s/\s+$//;
+                   $a_id =~ s/^\s+//;
+                   $a_id =~ s/\s+$//;
+                   $a_name =~ s/^\s+//;
+                   $a_name =~ s/\s+$//;
+                   $gene_name_hash{$a_id}  = $a_name;
 
                if ($gene_id_hash{$a_wb_gene_id}){
                    $gene_id_hash{$a_wb_gene_id} .= "AND" .  $a_id;
@@ -582,9 +699,8 @@ sub get_id_name_hash{
           my $single = @single_source_array;
            push(@single_source_array, $line);
        }
-  }
+#  }
  }
-
  my @db_lines = read_file($db_gene_list);
  foreach my $db_line (@db_lines){
   chomp($db_line);
@@ -596,8 +712,8 @@ sub get_id_name_hash{
         $Gene =~ s/\s+$//;
         $gene_name_hash{$WBGene} = $Gene;
      }
+  }
  }
-
  return \%gene_id_hash, \%gene_name_hash;
 }
 sub get_gene_array{
@@ -617,12 +733,13 @@ sub get_gene_array{
         push(@gene_array, $WBGene);
      }
  }
-      my @sorted_gene_array = uniq(sort(@gene_array));
+      my @sorted_gene_array=uniq(sort(@gene_array));
       return \@sorted_gene_array;
 }
 sub get_gene_name_hash{
  my $orthology = shift;
  my $db_gene_list = shift;
+
  my %gene_name_hash=();
  my @lines = read_file($orthology);
  foreach my $line (@lines){
@@ -651,4 +768,118 @@ sub get_gene_name_hash{
      }
  }
       return \%gene_name_hash;
+}
+sub get_biomart_description_hash{
+ my $file = shift;
+ my %hash=();
+# define some variables
+my $family = "\, family ";
+my $family2 = " family member";
+my $family3 = " family";
+my $family4 = " family\, member";
+my $uncharacterized = "uncharacterized";
+my $group = "\, group ";
+my $member = "\, member ";
+my $subfamily = "\, subfamily ";
+my $subfamily2 = "\, sub\-family ";
+my $subfamily3 = "subfamily";
+my $polypeptide ="\, polypeptide ";
+my $class = "\, class ";
+my $homolog  = "homolog ";
+my $homolog2 = "homolog";
+my $homolog3 = "homolog\.*";
+#
+   my $csv = Text::CSV->new ({
+     binary    => 1,
+     auto_diag => 1,
+     sep_char  => ','    # not really needed as this is the default
+   });
+   open(my $data, '<:encoding(utf8)', $file) or die "Could not open '$file' $!\n";
+   while (my $fields = $csv->getline( $data )) {
+ 
+  my $description =  $fields->[2];
+  my $ensg = $fields->[0];
+  my $hgnc =  $fields->[4];
+       $ensg =~ s/^\s+//;
+       $ensg =~ s/\s+$//;
+       $hgnc =~ s/^\s+//;
+       $hgnc =~ s/\s+$//;
+       
+  if ($ensg){
+      $ensg =~s/ENSEMBL\://gi;
+      if ($description !~/$uncharacterized/i){
+         $description =~ s/\[.+?\]//g;
+         $description =~ s/^\s+//;
+         $description =~ s/\s+$//;
+         $description =~s/$homolog//g;
+         $description =~s/$homolog2//g;
+# Rule - remove any reference to kDa 
+         $description =~s/\,\s\d*kDa//gi;
+         $description =~s/\d*kDa//gi;
+         $description =~s/\dkDa//gi;
+         $description =~s/\(\d*kDa\)//gi;
+         $description =~s/\(\d*kD\)//gi;
+         $description =~s/\(\d+.\d+kD\)//gi;
+         $description =~s/\(\d+.\d+kDa\)//gi;
+         $description =~s/\d+.\d+kDa//gi;
+# Rule - apply the same rules regarding family, subfamily, member in description names 
+         if ($description =~/$family4/) {
+             $description =~s/$family4//g;
+          if ($description =~/(\d+)\s(\S+)$/){
+              my $substring1 = $1;
+              my $substring2 = $2;
+              $description =~s/$substring1\s$substring2/$substring1$substring2/g;
+            }
+         }
+         if (($description =~/$member/) and ($description =~/$class/)){
+              $description =~ s/$class//g;
+          }
+         if ( (($description =~/$family/) and ($description =~/$subfamily/))or
+              (($description =~/$family/) and ($description =~/$group/))or
+              (($description =~/$family/) and ($description =~/$polypeptide/))or
+              (($description =~/$family/) and ($description =~/$member/))or
+              (($description =~/$subfamily/) and ($description =~/$group/))or
+              (($description =~/$subfamily/) and ($description =~/$polypeptide/))or
+              (($description =~/$subfamily/) and ($description =~/$member/))or
+              (($description =~/$subfamily2/) and ($description =~/$group/))or
+              (($description =~/$subfamily2/) and ($description =~/$polypeptide/))or
+              (($description =~/$subfamily2/) and ($description =~/$member/))or
+              (($description =~/$subfamily2/) and ($description =~/$family/))or
+              (($description =~/$group/) and ($description =~/$polypeptide/))or
+              (($description =~/$group/) and ($description =~/$member/))or
+              (($description =~/$member/) and ($description =~/$polypeptide/)) or 
+              (($description =~/$class/) and ($description =~/$subfamily/))or
+              (($description =~/$class/) and ($description =~/$group/))or
+              (($description =~/$class/) and ($description =~/$polypeptide/))or
+              (($description =~/$class/) and ($description =~/$member/))or
+              (($description =~/$class/) and ($description =~/$family/)) ) {
+
+      if ($description !~/$family/){
+              $description =~s/$subfamily\s?/ /g;
+              $description =~s/$subfamily2\s?/ /g;
+              $description =~s/$subfamily3\s?//g;
+       } else{
+              $description =~s/$subfamily//g;
+              $description =~s/$subfamily2//g;
+       }
+      
+      $description =~s/$family\s?/ /g;
+      $description =~s/$family2\s?/ /g;
+      $description =~s/$subfamily\s?/ /g;
+      $description =~s/$subfamily2\s?/ /g;
+      $description =~s/$group\s?//g;
+      $description =~s/$member\s?//g;
+      $description =~s/$polypeptide\s?//g;
+      $description =~s/$family3//g;
+         if ($description =~/\)(\d+)$/){
+              my $substring1 = $1;
+              $description =~s/\)$substring1/\) $substring1/g;
+          }
+        }
+          $hash{$hgnc} = $description;
+      }
+  }
+ }
+ close($data);
+ return \%hash;
 }
